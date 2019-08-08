1a906e0668d27906aa285cacbc91cd0ce782db3e
Vlad Bezden <vlad.bezden@gmail.com>
2019-08-07 22:01:55 -0400

    closed #1

---
+++ hello-world/hello_world.go
@@ -0,0 +1,8 @@
+// The classical introductory exercise. Just say "Hello, World!".
+
+package greeting
+
+// HelloWorld function greets with Hello World message
+func HelloWorld() string {
+	return "Hello, World!"
+}
