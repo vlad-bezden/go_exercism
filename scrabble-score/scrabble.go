ad9a41b2ce1654118aace1f094130d9782defbb5
Vlad Bezden <vlad.bezden@gmail.com>
2019-08-22 20:31:12 -0400

    closed #7

---
+++ scrabble-score/scrabble.go
@@ -0,0 +1,41 @@
+//Package scrabble provides Exercism solution for "Scrabble Score" exercise
+package scrabble
+
+import "strings"
+
+var dict = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
+}
+
+//Score function computes the scrabble score for a given word.
+func Score(word string) (n int) {
+	for _, c := range strings.ToUpper(word) {
+		n += dict[c]
+	}
+	return n
+}
