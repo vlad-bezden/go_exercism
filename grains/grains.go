5c89d2139206e82d19d04b547ea58026cdd923a4
Vlad Bezden <vlad.bezden@gmail.com>
2019-08-30 10:21:56 -0400

    Final solution to the problem

--- grains/grains.go
+++ grains/grains.go
@@ -3,16 +3,16 @@ package grains
 
 import "errors"
 
-//Square calculate the number of grains of wheat on n square of chessboard
+//Square calculates the number of grains of wheat on n square of chessboard
 func Square(n int) (uint64, error) {
 	if n <= 0 || 64 < n {
-		return 0, errors.New("invalid input. Valid values 0-64")
+		return 0, errors.New("invalid input. Valid values 1-64")
 	}
 
 	return 1 << uint(n-1), nil
 }
 
-//Total return total number of grains
+//Total calculates total number of grains
 func Total() uint64 {
-	return 1<<uint(64) - 1
+	return ^uint64(0)
 }
